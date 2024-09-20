f3fb541e22b5dc9b9080d693ad4d613a262cda09
mahdihasan28 <mahdihasan28@gmail.com>
2024-09-20 10:47:03 +0600

    Public application edit

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -53,18 +53,20 @@ class ApplicationController extends Controller
     public function publicShow(Request $request)
     {
         $request->validate([
-            'application_id' => 'required|exists:applications,id',
-            'institute_id' => 'required|exists:institutes,id',
+            'application_id' => 'required',
+            'institute_code' => 'required',
         ]);
 
         $application = Application::query()
             ->where('id', $request->application_id)
-            ->where('institute_id', $request->institute_id)
+            ->whereHas('institute', function ($query) use ($request) {
+                $query->where('institute_code', $request->institute_code);
+            })
             ->with([
                 'exam:id,name',
                 'zamat:id,name',
-                'area:id,name',
-                'institute:id,name',
+                'area:id,name,area_code',
+                'institute:id,name,institute_code',
                 'center',
                 'submittedBy',
                 'approvedBy',
@@ -89,7 +91,7 @@ class ApplicationController extends Controller
             'area_id' => 'nullable|exists:areas,id',
             'center_id' => 'nullable|exists:institutes,id',
             
-            'gender' => 'required|in:male,female',
+            // 'gender' => 'required|in:male,female',
 
             'students' => 'required|array|min:1',
 
@@ -112,7 +114,7 @@ class ApplicationController extends Controller
                 'zamat_id' => $request->zamat_id,
                 'group_id' => $request->group_id,
                 'center_id' => $request->center_id,
-                'gender' => $request->gender,
+                // 'gender' => $request->gender,
                 'payment_status' => 'Pending',
                 'total_amount' => $request->total_amount,
                 'payment_method' => $request->payment_method ?? 'Offline',
@@ -200,9 +202,13 @@ class ApplicationController extends Controller
 
         try {
             $application = self::$application ?? Application::findOrFail($id);
-            $application->update(['payment_status' => $request->payment_status]);
 
-            if ($request->payment_status === 'Paid') {
+            if ($request->payment_status === 'Paid' && !Student::where('application_id', $application->id)->exists()) {
+                $registration_numbers = Student::query()
+                    ->where('exam_id', $application->exam_id)
+                    ->pluck('registration_number')
+                    ->toArray();
+
                 foreach ($application->students as $studentData) {
                     Student::create([
                         'application_id' => $application->id,
@@ -218,11 +224,13 @@ class ApplicationController extends Controller
                         'father_name_arabic' => $studentData['father_name_arabic'],
                         'date_of_birth' => $studentData['date_of_birth'],
                         'address' => $studentData['address'],
-                        'gender' => $application->gender,
-                        'registration_number' => $this->generateRegistrationNumber(),
+                        // 'gender' => $application->gender,
+                        'registration_number' => $this->generateRegistrationNumber($application->exam_id, $registration_numbers),
                     ]);
                 }
             }
+
+            $application->update(['payment_status' => $request->payment_status]);
 
             return response()->json(['message' => 'Payment status updated successfully']);
         } catch (\Exception $e) {
@@ -230,8 +238,16 @@ class ApplicationController extends Controller
         }
     }
 
-    private function generateRegistrationNumber()
+    private function generateRegistrationNumber($exam_id, &$previous_registration_numbers)
     {
-        return str_pad(mt_rand(1, 99999999), 8, '0', STR_PAD_LEFT);
-    }
+        do {
+            $rand = rand(100000, 999999); // Random number
+            $new_registration_number = $exam_id . $rand;
+        } while (in_array($new_registration_number, $previous_registration_numbers));
+    
+        // Push the new registration number into the array
+        $previous_registration_numbers[] = $new_registration_number;
+    
+        return $new_registration_number;
+    }    
 }
